d4346b937eccdaecbd69e2426a65503fcf6cb7eb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:45:15 +0200

    Add person repo interface

---
+++ AspCoreApp/Data/Repositories/Abstract/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using AspCoreApp.Models;
+
+namespace AspCoreApp.Data.Repositories.Abstract
+{
+    public interface IPersonRepository
+    {
+        IList<Person> GetAll();
+        Person GetById(string personId);
+        void Add(Person person);
+        void Update(Person person);
+        void Delete(string personId);
+    }
+}
