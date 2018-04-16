a86e936c09556014838c1f968699dc3c5eca42f0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:15:28 +0200

    Scaffold MVC Entity controller with views

---
+++ AspCoreApp/Controllers/AddressController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AspCoreApp.Data;
+using AspCoreApp.Models;
+
+namespace AspCoreApp.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AddressController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Address
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Addresses.ToListAsync());
+        }
+
+        // GET: Address/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Address/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Street,Number")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        // GET: Address/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses.SingleOrDefaultAsync(m => m.Id == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Address/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Street,Number")] Address address)
+        {
+            if (id != address.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        // GET: Address/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Address/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var address = await _context.Addresses.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressExists(string id)
+        {
+            return _context.Addresses.Any(e => e.Id == id);
+        }
+    }
+}
