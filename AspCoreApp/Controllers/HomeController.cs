3a55db0b34291de6b8e913271da7fedb3437f9df
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:16:50 +0200

    Remove about + contact actions and views

--- AspCoreApp/Controllers/HomeController.cs
+++ AspCoreApp/Controllers/HomeController.cs
@@ -11,23 +11,14 @@ namespace AspCoreApp.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var vm = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(vm);
         }
     }
 }
