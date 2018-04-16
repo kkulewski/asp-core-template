f96b2be8a5aeb8a9eac9aa07d0d3cb4857524a13
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:24:14 +0200

    Add address repo impl draft

---
+++ AspCoreApp/Data/Repositories/AddressRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using AspCoreApp.Data.Repositories.Abstract;
+using AspCoreApp.Models;
+
+namespace AspCoreApp.Data.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private AppDbContext _context;
+
+        public AddressRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Address> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
