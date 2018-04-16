033fdff7372d77f6ac686730f286749524789549
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:55:26 +0200

    Add person repo impl

--- AspCoreApp/Data/Repositories/PersonRepository.cs
+++ AspCoreApp/Data/Repositories/PersonRepository.cs
@@ -1,35 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AspCoreApp.Data.Repositories.Abstract;
 using AspCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspCoreApp.Data.Repositories
 {
     public class PersonRepository : IPersonRepository
     {
+        private readonly AppDbContext _context;
+
+        public PersonRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IList<Person> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.People
+                .Include(x => x.Address)
+                .ToList();
         }
 
         public Person GetById(string personId)
         {
-            throw new NotImplementedException();
+            return _context.People
+                .Include(x => x.Address)
+                .FirstOrDefault(x => x.Id == personId);
         }
 
         public void Add(Person person)
         {
-            throw new NotImplementedException();
+            _context.People.Add(person);
+            _context.SaveChanges();
         }
 
         public void Update(Person person)
         {
-            throw new NotImplementedException();
+            _context.People.Update(person);
+            _context.SaveChanges();
         }
 
         public void Delete(string personId)
         {
-            throw new NotImplementedException();
+            var person = _context.People.FirstOrDefault(x => x.Id == personId);
+            if (person != null)
+            {
+                _context.People.Remove(person);
+                _context.SaveChanges();
+            }
         }
     }
 }
