b6a7b7a77c55444d918e11b20826f4243bbe18f3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:22:15 +0200

    Add address repo interface

---
+++ AspCoreApp/Data/Repositories/Abstract/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using AspCoreApp.Models;
+
+namespace AspCoreApp.Data.Repositories.Abstract
+{
+    public interface IAddressRepository
+    {
+        IList<Address> GetAll();
+        Address GetById(string id);
+        void Add(Address address);
+        void Update(Address address);
+        void Delete(string id);
+    }
+}
