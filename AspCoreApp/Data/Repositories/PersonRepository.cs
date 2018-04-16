e5084484a3dca28e27a8b8b41fae5a26adebcf6f
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 18:50:55 +0200

    Add person repo impl draft

---
+++ AspCoreApp/Data/Repositories/PersonRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AspCoreApp.Data.Repositories.Abstract;
+using AspCoreApp.Models;
+
+namespace AspCoreApp.Data.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        public IList<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetById(string personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string personId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
