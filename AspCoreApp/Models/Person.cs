39b0d9e8e8a08440b5a317b076d307a2e7f3e572
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-16 19:10:59 +0200

    Add person validators

--- AspCoreApp/Models/Person.cs
+++ AspCoreApp/Models/Person.cs
@@ -1,11 +1,23 @@
-﻿namespace AspCoreApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspCoreApp.Models
 {
     public class Person
     {
         public string Id { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
         public Address Address { get; set; }
+
         public string AddressId { get; set; }
     }
 }
