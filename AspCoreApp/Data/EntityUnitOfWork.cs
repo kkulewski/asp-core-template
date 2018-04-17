2282a4c4ac160e9fdadf7ec95e2fc4c7873a6185
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-18 00:41:28 +0200

    Add unit of work + impl

---
+++ AspCoreApp/Data/EntityUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace AspCoreApp.Data
+{
+    public class EntityUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public EntityUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AspCoreApp/Data/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace AspCoreApp.Data
+{
+    /// <summary>
+    /// Unit of work pattern is a single access point to persistence service - most often a database.
+    /// It enables features like transactions and rollbacks.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Save changes in persistence service.
+        /// It should be called after each transaction.
+        /// </summary>
+        Task SaveChanges();
+    }
+}
